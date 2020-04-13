04f448643e118dc30aea655ca4758bb29d583282
Crouston <yosuam34@gmail.com>
2020-04-13 16:45:25 +0700

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject firstButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstButton.GetComponent<Selectable>().Select();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
