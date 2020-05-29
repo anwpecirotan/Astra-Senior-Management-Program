6ffbed8c598ba1a1b556559b6b05951ab6cd0591
Crouston <yosuam34@gmail.com>
2020-05-29 17:18:17 +0700

    updated

---
+++ Assets/Scripts/InformationFormulaData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InformationFormulaData : MonoBehaviour
+{
+    public static string information = "empty" ;
+    public TextMeshProUGUI textUI;
+    // Update is called once per frame
+    void Update()
+    {
+        textUI.text = information; 
+    }
+}
---
+++ Assets/Scripts/InformationHolder.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class InformationHolder : MonoBehaviour,IPointerEnterHandler
+{
+    public string information;
+
+    public GameObject popupWindowObject;
+
+    private void Start()
+    {
+        popupWindowObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("test");
+        InformationFormulaData.information = information;
+    }
+
+    
+}
